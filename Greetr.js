2df4ebf81ca420f790a0985839e1e19a95cd58b8
davidmgudeman <davidmgudeman@gmail.com>
2017-06-13 04:18:16 -0700

    added encapsulated, chainable methods

--- Greetr.js
+++ Greetr.js
@@ -3,7 +3,77 @@
     var Greetr = function(firstName, lastName, language){
         return new Greetr.init(firstName, lastName, language);
     }
-    Greetr.prototype = {};
+   
+    // Not exposed ------------------------------------------
+    var supportedLangs = ['en', 'es'];
+   
+    var greetings = {
+        en: 'Hello',
+        es: 'Hola'
+    };
+
+    var formalGreetings ={
+        en: 'Greetings',
+        es: 'Saludos'
+    };
+
+    var logMessages = {
+        en: 'Logged in',
+        es: 'Inició sesión'
+    }
+    // End Not exposed ---------------------------------------
+    
+    // Exposed ------------------------------------------------
+    Greetr.prototype = {
+        fullName: function() {
+            return this.firstName + ' ' + this.lastName;
+        },
+        validate: function() {
+           if (supportedLangs.indexOf(this.language) === -1) {
+                throw "Invalid language"
+            }
+        },
+
+        greeting: function() {
+            return greetings[this.language] + ' ' + this.firstName + '!'
+        },
+
+        formalGreeting: function() {
+            return formalGreetings[this.language] + ', ' + this.fullName();
+        },
+
+        greet: function(formal) {
+            var msg;
+            if (formal) {
+                msg = this.formalGreeting();
+            } else {
+                msg = this.greeting();
+            }
+
+            if (console) {
+                console.log(msg);
+            }
+            // 'this' refers to the calling object at execution time
+            // makes the method chainable
+            return this;
+        },
+
+        log: function() {
+            // if (console) is for IE because it only has the console var when open
+            if (console) {
+                console.log(logMessages[this.language] + ': ' + this.fullName());
+            }
+            return this; // make chainable
+        },
+
+        setLang: function(lang) {
+            this.language = lang;
+            this.validate();
+            return this;  // make chainable
+        }
+    };
+    // end Greetr.prototype -------------------------------------
+
     Greetr.init = function(firstName, lastName, language){
         var self = this;
         self.firstName = firstName || 'Dave';
